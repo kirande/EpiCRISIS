428557db38ba9c5cbc3f99748112fc051a03e286
Svetlozar Todorov <svetlik@gmail.com>
2012-01-10 23:29:56 +0200

    Generate a default Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EpiCRISIS::Application.config.secret_token = '8c590df37ec2a2b8ff22ed47994a89cb4ad50f776b30fc32a1fe535431a5c69ecebf3611fc00cbb27ab9977c90b0f998796d5bfd86c05d6bc2ee7dd4d693fc28'
